aa645c81e2c77efd44422c7cc51637e94db6f353
Xu Yifeng <yfxu@NetEase>
2023-06-27 11:27:10 +0800

    build_options: remove unused options std::vector<Option> get_mgr_options(); std::vector<Option> get_mon_options(); std::vector<Option> get_crimson_options(); std::vector<Option> get_osd_options(); std::vector<Option> get_rgw_options(); std::vector<Option> get_rbd_options(); std::vector<Option> get_rbd_mirror_options(); std::vector<Option> get_immutable_object_cache_options(); std::vector<Option> get_mds_options(); std::vector<Option> get_mds_client_options(); std::vector<Option> get_cephfs_mirror_options(); std::vector<Option> get_ceph_exporter_options()

--- src/common/options/build_options.cc
+++ src/common/options/build_options.cc
@@ -7,19 +7,19 @@
 #include <cstring>
 
 std::vector<Option> get_global_options();
-std::vector<Option> get_mgr_options();
-std::vector<Option> get_mon_options();
-std::vector<Option> get_crimson_options();
-std::vector<Option> get_osd_options();
-std::vector<Option> get_rgw_options();
-std::vector<Option> get_rbd_options();
-std::vector<Option> get_rbd_mirror_options();
-std::vector<Option> get_immutable_object_cache_options();
-std::vector<Option> get_mds_options();
-std::vector<Option> get_mds_client_options();
-std::vector<Option> get_cephfs_mirror_options();
-std::vector<Option> get_ceph_exporter_options();
-
+//std::vector<Option> get_mgr_options();
+//std::vector<Option> get_mon_options();
+//std::vector<Option> get_crimson_options();
+//std::vector<Option> get_osd_options();
+//std::vector<Option> get_rgw_options();
+//std::vector<Option> get_rbd_options();
+//std::vector<Option> get_rbd_mirror_options();
+//std::vector<Option> get_immutable_object_cache_options();
+//std::vector<Option> get_mds_options();
+//std::vector<Option> get_mds_client_options();
+//std::vector<Option> get_cephfs_mirror_options();
+//std::vector<Option> get_ceph_exporter_options();
+//
 std::vector<Option> build_options()
 {
   std::vector<Option> result = get_global_options();
@@ -36,18 +36,18 @@ std::vector<Option> build_options()
     }
   };
 
-  ingest(get_crimson_options(), "osd");
-  ingest(get_mgr_options(), "mgr");
-  ingest(get_mon_options(), "mon");
-  ingest(get_osd_options(), "osd");
-  ingest(get_rgw_options(), "rgw");
-  ingest(get_rbd_options(), "rbd");
-  ingest(get_rbd_mirror_options(), "rbd-mirror");
-  ingest(get_immutable_object_cache_options(), "immutable-object-cache");
-  ingest(get_mds_options(), "mds");
-  ingest(get_mds_client_options(), "mds_client");
-  ingest(get_cephfs_mirror_options(), "cephfs-mirror");
-  ingest(get_ceph_exporter_options(), "ceph-exporter");
-
+//  ingest(get_crimson_options(), "osd");
+//  ingest(get_mgr_options(), "mgr");
+//  ingest(get_mon_options(), "mon");
+//  ingest(get_osd_options(), "osd");
+//  ingest(get_rgw_options(), "rgw");
+//  ingest(get_rbd_options(), "rbd");
+//  ingest(get_rbd_mirror_options(), "rbd-mirror");
+//  ingest(get_immutable_object_cache_options(), "immutable-object-cache");
+//  ingest(get_mds_options(), "mds");
+//  ingest(get_mds_client_options(), "mds_client");
+//  ingest(get_cephfs_mirror_options(), "cephfs-mirror");
+//  ingest(get_ceph_exporter_options(), "ceph-exporter");
+//
   return result;
 }
